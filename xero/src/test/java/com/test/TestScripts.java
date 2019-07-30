8b348a955a273a565eabc69d0db7fd21654d833e
SeleTesting <47877204+SeleTesting@users.noreply.github.com>
2019-07-30 14:16:01 -0700

    Add files via upload

--- xero/src/test/java/com/test/TestScripts.java
+++ xero/src/test/java/com/test/TestScripts.java
@@ -13,32 +13,32 @@ public class TestScripts extends Reuse
 {
 public static void main(String[] args) throws Exception 
 {	
-//	LoginPage a=new LoginPage();
-//	a.Tc1A();
-//	a.Tc1B();
-//	a.Tc1C();
-//	a.Tc1D();
-//	SignUpPage b=new SignUpPage();
-//	b.Tc2A();
-//	b.Tc2B();
-//	b.Tc2C();
-//	b.Tc2D();
-//	b.Tc2E();
-//	NavigationPage c=new NavigationPage();
-//	c.Tc3();
-//	LogoutPage d=new LogoutPage();
-//	d.Tc4();
-//	CreateAccountPage e=new CreateAccountPage();
-//	e.Tc5();
+	LoginPage a=new LoginPage();
+	a.Tc1A();
+	a.Tc1B();
+	a.Tc1C();
+	a.Tc1D();
+	SignUpPage b=new SignUpPage();
+	b.Tc2A();
+	b.Tc2B();
+	b.Tc2C();
+	b.Tc2D();
+	b.Tc2E();
+	NavigationPage c=new NavigationPage();
+	c.Tc3();
+	LogoutPage d=new LogoutPage();
+	d.Tc4();
+	CreateAccountPage e=new CreateAccountPage();
+	e.Tc5();
 	ProfilePage f=new ProfilePage();
 	f.Tc6();
-//	MyXeroPage g=new MyXeroPage();
-//	g.Tc8A();
-//	g.Tc8B();
-//   g.Tc8C();
-//	g.Tc8F();
-//	AccountsPage h=new AccountsPage();
-//	h.Tc10();
+	MyXeroPage g=new MyXeroPage();
+	g.Tc8A();
+	g.Tc8B();
+   g.Tc8C();
+	g.Tc8F();
+	AccountsPage h=new AccountsPage();
+	h.Tc10();
     
 }
 }
